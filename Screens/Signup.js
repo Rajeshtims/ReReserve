e2abf2c6d5802360511fb3261639e99206812e51
Cal <khalfanijabari@gmail.com>
2022-11-09 19:56:58 -0500

    Map pins

--- Screens/Signup.js
+++ Screens/Signup.js
@@ -1,16 +1,62 @@
+// https://reactjs.org/docs/hooks-effect.html
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, TextInput, Dimensions} from 'react-native';
-import {useNavigation} from '@react-navigation/native'; // Key to navigations in some components
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// https://reactnative.dev/docs/components-and-apis
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Dimensions,
+  PermissionsAndroid,
+  Platform,
+} from 'react-native';
+// https://reactnativepaper.com/
 import {Button} from 'react-native-paper';
+// https://reactnavigation.org/docs/getting-started
+import {useNavigation} from '@react-navigation/native';
+// https://github.com/react-native-async-storage/async-storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// https://github.com/Agontuk/react-native-geolocation-service
+import Geolocation from 'react-native-geolocation-service';
 
 export default function Signup() {
-  const navigation = useNavigation(); // Key to navigations in some components
+  // Key to navigation:
+  const navigation = useNavigation();
+  // Init. state variables:
   const [isLoading, setIsLoading] = useState(true);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [venmoID, setVenmoID] = useState();
   const [venmoPassword, setVenmoPassword] = useState();
+  const [location, setLocation] = useState(null);
+  const [storedEmail, setStoredEmail] = useState(null);
+
+  // Function to get user's current Android location:
+  const requestAndroidLocation = async () => {
+    try {
+      let access = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Geolocation Permission',
+          message: 'Can we access your location?',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // Function to get user's current iPhone location:
+  const requestIphoneLocation = async () => {
+    const access = await Geolocation.requestAuthorization('always').then(
+      res => {
+        console.log(res);
+      },
+    );
+  };
+
   // Similiar to listeners:
   const onChangeEmail = text => {
     setEmail(text);
@@ -24,7 +70,7 @@ export default function Signup() {
   const onChangeVenmoPassword = text => {
     setVenmoPassword(text);
   };
-  // Save the input locally:
+  // Save the input local DB:
   const saveLocalData = async () => {
     try {
       AsyncStorage.setItem(`email`, email);
@@ -36,11 +82,11 @@ export default function Signup() {
     }
     navigation.navigate('Home');
   };
-  // Fetch local data:
+  // Fetch local data from local DB:
   const getLocalData = async () => {
     try {
       const email = await AsyncStorage.getItem(`email`);
-      if (email != null) navigation.navigate('Home');
+      setStoredEmail(email);
     } catch (error) {
       console.log(error);
     }
@@ -48,9 +94,18 @@ export default function Signup() {
   };
 
   useEffect(() => {
-    // Check if user already registed, if so... forward to home screen:
-    if (isLoading) getLocalData();
-  }, []);
+    // Get location permission, and previously stored data:
+    if (isLoading) {
+      if (Platform.OS == 'ios') requestIphoneLocation();
+      if (Platform.OS == 'android') requestAndroidLocation();
+      getLocalData();
+    }
+    // Check if user already registered, if so... forward to home screen:
+    if (storedEmail != null)
+      navigation.navigate('Home', {
+        location: location,
+      });
+  }, [location, storedEmail]); // Reload component if these vars change
   return (
     <View style={styles.main}>
       <View style={styles.inputContainer}>
@@ -58,24 +113,28 @@ export default function Signup() {
           style={styles.input}
           value={email}
           placeholder="Email: "
+          placeholderTextColor={'#455A64'}
           onChangeText={text => onChangeEmail(text)}
         />
         <TextInput
           style={styles.input}
           value={password}
           placeholder="Password: "
+          placeholderTextColor={'#455A64'}
           onChangeText={text => onChangePassword(text)}
         />
         <TextInput
           style={styles.input}
           value={venmoID}
           placeholder="Venmo ID: "
+          placeholderTextColor={'#455A64'}
           onChangeText={text => onChangeVenmoID(text)}
         />
         <TextInput
           style={styles.input}
           value={venmoPassword}
           placeholder="Venmo Password: "
+          placeholderTextColor={'#455A64'}
           onChangeText={text => onChangeVenmoPassword(text)}
         />
       </View>
@@ -89,7 +148,7 @@ export default function Signup() {
     </View>
   );
 }
-
+// Styles to use throughout the component:
 const styles = StyleSheet.create({
   main: {
     height: Dimensions.get('window').height,
