ac73f5683c716e28a2d89728cec21e4d6f76a507
Cal <khalfanijabari@gmail.com>
2022-11-15 16:58:25 -0500

    buy screen, reservations screen, confirmation

--- Screens/PurchaseOverview.js
+++ Screens/PurchaseOverview.js
@@ -6,48 +6,50 @@ import Buy from './Buy';
 import {restaurant, date} from './Reservations';
 
 export default function Purchase({route}) {
-    const navigation = useNavigation();
-  
-    useEffect(() => {
-      console.log('ConfirmPurchase Screen: ');
-      
-    }, []);
-    return (
-      <View style={styles.main}>
-        <Text style={styles.fontSize}>restaurant place holder</Text>
-        <Text style={styles.fontSize}>table size place holder</Text>
-        <Text style={styles.fontSize}>date place holder</Text>
-        <Button style={styles.sellButton} onPress={() => navigation.navigate('Confirmation')}>
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    console.log('ConfirmPurchase Screen: ');
+    console.log(route.params.id);
+  }, []);
+  return (
+    <View style={styles.main}>
+      <Text style={styles.fontSize}>{route.params.restaurantName}</Text>
+      <Text style={styles.fontSize}>{route.params.tableSize}</Text>
+      <Text style={styles.fontSize}>{route.params.price}</Text>
+      <Text style={styles.fontSize}>date place holder</Text>
+      <Button
+        style={styles.sellButton}
+        onPress={() => navigation.navigate('Confirmation')}>
         <Text style={{color: 'black', fontWeight: '400', fontSize: 20}}>
           {'Buy Now!'}
         </Text>
       </Button>
-      </View>
-    );
+    </View>
+  );
 }
-    const styles = StyleSheet.create({
-        main: {
-          height: Dimensions.get('window').height,
-          width: Dimensions.get('window').width,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-        sellButton: {
-            marginTop: '15%',
-            backgroundColor: 'green',
-            width: Platform.OS == 'ios' ? 300 : '80%',
-            height: Platform.OS == 'ios' ? 50 : null,
-            display: 'flex',
-            justifyContent: 'center',
-            shadowColor: '#171717',
-            shadowOffset: {width: -2, height: 4},
-            shadowOpacity: 0.2,
-            shadowRadius: 5,
-          },
-          fontSize: {
-            fontSize: 24,
-            padding: 20,
-          }
-      });
-  
+const styles = StyleSheet.create({
+  main: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sellButton: {
+    marginTop: '15%',
+    backgroundColor: 'green',
+    width: Platform.OS == 'ios' ? 300 : '80%',
+    height: Platform.OS == 'ios' ? 50 : null,
+    display: 'flex',
+    justifyContent: 'center',
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  fontSize: {
+    fontSize: 24,
+    padding: 20,
+  },
+});
